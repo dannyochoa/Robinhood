6834679a9583de38e825b68b9a82f7ac9b831600
unknown <dochoa-aguila@csumb.edu>
2020-01-29 22:42:33 -0800

    myFunc page added and moved get price function there

--- Robinhood/dev.py
+++ Robinhood/dev.py
@@ -1,13 +1,14 @@
 from Robinhood import Robinhood
-import statistics
+from myFunc import get_Prices
 
 my_trader = Robinhood()
 try_signin = True
 while(try_signin):
     try:
+        my_trader.login(username="", password="")
         try_signin = False
         signin = True
-        
+
     except:
         retry = input("Error Logging in! \n Would you like to try Again (y) (n)")
         if(retry == 'n'):
@@ -16,58 +17,3 @@ while(try_signin):
 
 if(signin):
     get_Prices('APPL')
-
-
-
-
-
-
-
-
-
-
-def get_Prices(symbol):
-    stock_instrument = my_trader.instruments(symbol)[0]
-
-    week1 = my_trader.get_historical_quotes(symbol, '5minute','week')
-
-    prices = week1['results'][0]['historicals']
-
-    low = 99999999;
-    high = 0;
-    medianArr = []
-
-    for price in prices:
-        open_price = float(price['open_price'])
-        close_price = float(price['close_price'])
-        high_price = float(price['high_price'])
-        low_price = float(price['low_price'])
-        if(open_price < low):
-            low = open_price
-        if(open_price > high):
-            high = open_price
-        if(close_price < low):
-            low = close_price
-        if(close_price > high):
-            high = close_price
-        if(high_price < low):
-            low = high_price
-        if(high_price > high):
-            high = high_price
-        if(low_price < low):
-            low = low_price
-        if(low_price > high):
-            high = low_price
-        medianArr.insert(len(medianArr),open_price)
-        medianArr.insert(len(medianArr),close_price)
-        medianArr.insert(len(medianArr),high_price)
-        medianArr.insert(len(medianArr),low_price)
-
-    print("********************** Company **************************")
-    print(symbol)
-    print("************************ LOW ****************************")
-    print(low)
-    print("*********************** High ****************************")
-    print(high)
-    print("*********************** median **************************")
-    print(statistics.median(medianArr))
---
+++ Robinhood/myFunc.py
@@ -0,0 +1,47 @@
+import statistics
+
+def get_Prices(my_trader, symbol):
+    stock_instrument = my_trader.instruments(symbol)[0]
+
+    week1 = my_trader.get_historical_quotes(symbol, '5minute','week')
+
+    prices = week1['results'][0]['historicals']
+
+    low = 99999999;
+    high = 0;
+    medianArr = []
+
+    for price in prices:
+        open_price = float(price['open_price'])
+        close_price = float(price['close_price'])
+        high_price = float(price['high_price'])
+        low_price = float(price['low_price'])
+        if(open_price < low):
+            low = open_price
+        if(open_price > high):
+            high = open_price
+        if(close_price < low):
+            low = close_price
+        if(close_price > high):
+            high = close_price
+        if(high_price < low):
+            low = high_price
+        if(high_price > high):
+            high = high_price
+        if(low_price < low):
+            low = low_price
+        if(low_price > high):
+            high = low_price
+        medianArr.insert(len(medianArr),open_price)
+        medianArr.insert(len(medianArr),close_price)
+        medianArr.insert(len(medianArr),high_price)
+        medianArr.insert(len(medianArr),low_price)
+
+    print("********************** Company **************************")
+    print(symbol)
+    print("************************ LOW ****************************")
+    print(low)
+    print("*********************** High ****************************")
+    print(high)
+    print("*********************** median **************************")
+    print(statistics.median(medianArr))
